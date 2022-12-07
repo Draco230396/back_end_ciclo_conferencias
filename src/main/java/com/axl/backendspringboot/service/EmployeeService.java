420e2ba50aa42c47cab8538fb47f42ef7e9ece70
Romeo Cuahua Galvez <romeo.cuahua@neoris.com>
2022-12-07 16:43:18 -0600

    CODIGO FUNCIONAL CRUD - Spring Boot

---
+++ src/main/java/com/axl/backendspringboot/service/EmployeeService.java
@@ -0,0 +1,14 @@
+/*
+package com.axl.backendspringboot.service;
+
+import com.axl.backendspringboot.model.Employee;
+import java.util.List;
+
+public interface EmployeeService {
+	Employee saveEmployee(Employee employee);
+	List<Employee> getAllEmployees();
+	Employee getEmployeeById(long id);
+	Employee updateEmployee(Employee employee, long id);
+	void deleteEmployee(long id);
+}
+*/
